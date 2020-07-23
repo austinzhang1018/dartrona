9ceef30a31d501354926c3d883042560c9a9db93
Austin Zhang <austinzhang1018@gmail.com>
2020-07-23 18:08:05 -0400

    added frontend and cors

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
+
+from flask_cors import CORS, cross_origin
 from bot import get_prediction
 
-@app.route('/', methods['POST'])
+cors = CORS(app)
+
+@app.route('/', methods['GET'])
+def index():
+    return redirect('https://austinzhang1018.github.io/dartrona/')
+
+@app.route('/api-qa', methods['POST'])
 def question_answer():
     question = request.json['question']
     return get_prediction(question, 5, 5)
