2ac884a5cff289edd56693838dda6c1abdfcefe0
Austin Zhang <austinzhang1018@gmail.com>
2020-07-23 17:43:37 -0400

    Updates

---
+++ precache-manifest.ab1d16544b0af55e8ec5362fa286b7eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2594a16c2551bee4e81e6bcb5c3b1b76",
+    "url": "/austinzhang1018/dartrona/index.html"
+  },
+  {
+    "revision": "1028a8d0aa15d27a1065",
+    "url": "/austinzhang1018/dartrona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "370c9fd060cfe3a4ac85",
+    "url": "/austinzhang1018/dartrona/static/js/2.6d20342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/austinzhang1018/dartrona/static/js/2.6d20342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1028a8d0aa15d27a1065",
+    "url": "/austinzhang1018/dartrona/static/js/main.4ab7c38d.chunk.js"
+  },
+  {
+    "revision": "f3e7150a239dedfff321",
+    "url": "/austinzhang1018/dartrona/static/js/runtime-main.0d23091d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/austinzhang1018/dartrona/static/media/logo.5d5d9eef.svg"
+  }
+]);
